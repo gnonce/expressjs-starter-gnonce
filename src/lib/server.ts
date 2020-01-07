32f841d740c18c4fcefe5145bae2333a00469852
Kai-Markus Lehtimäki <kaikka@gnonce.com>
2020-01-07 18:59:28 +0200

    add logger

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -2,36 +2,32 @@
  * Main entry point for application. Set up server.
  */
 
-import express from "express";
-import { Request, Response } from 'express'
+import express from 'express';
+import morgan from 'morgan';
+import { Request, Response } from 'express';
 
 // Middleware
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 
 // Routes
-import hello from "../routes/hello";
+import hello from '../routes/hello';
 
 // Init express app
 const app = express();
-const PORT = parseInt(process.env.PORT) || 2500;
-const HOST = process.env.HOST || "0.0.0.0";
+
+// Logging
+app.use(morgan('combined'));
 
 // body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // use api version 1
-app.use("/api/v1", hello);
+app.use('/api/v1', hello);
 
 // catch all other routes
-app.all("*", (request: Request, response: Response) => {
+app.all('*', (request: Request, response: Response) => {
   return response.sendStatus(404);
 });
-
-export const start = () => {
-  app.listen(PORT, HOST, () => {
-    console.log(`REST API: listening on port ${PORT}`);
-  });
-};
 
 export default app;
